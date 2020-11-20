d24c09e1e2cd93204db78c7e7219b0efc18c6cb0
MARQyie <50098649+MARQyie@users.noreply.github.com>
2020-11-20 11:52:24 +0100

    Weekly update 2020-11-20

--- KNN_proxies_v1.py
+++ KNN_proxies_v1.py
@@ -18,7 +18,8 @@ import seaborn as sns
 sns.set(style = 'whitegrid', font_scale = 2)
 
 # Machine learning packages
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans, AgglomerativeClustering
+from scipy.cluster.hierarchy import dendrogram, linkage
 
 # Set WD
 import os
@@ -208,3 +209,40 @@ text_ss_latex = open('Tables/Cluster_centroids_control.tex', 'w')
 text_ss_latex.write(df_clus_oth_latex)
 text_ss_latex.close()
 
+#--------------------------------------------
+# EXTRA Make a Dendrogram
+#--------------------------------------------
+
+# Plot function
+def plot_dendrogram(model, **kwargs):
+    # Create linkage matrix and then plot the dendrogram
+
+    # create the counts of samples under each node
+    counts = np.zeros(model.children_.shape[0])
+    n_samples = len(model.labels_)
+    for i, merge in enumerate(model.children_):
+        current_count = 0
+        for child_idx in merge:
+            if child_idx < n_samples:
+                current_count += 1  # leaf node
+            else:
+                current_count += counts[child_idx - n_samples]
+        counts[i] = current_count
+
+    linkage_matrix = np.column_stack([model.children_, model.distances_,
+                                      counts]).astype(float)
+
+    # Plot the corresponding dendrogram
+    dendrogram(linkage_matrix, **kwargs)
+    
+# Setup the model
+model = AgglomerativeClustering(distance_threshold=0, n_clusters=None)
+
+# Fit model
+model = model.fit(df[vars_tot + vars_oth])
+plt.title('Hierarchical Clustering Dendrogram')
+
+# Plot
+plot_dendrogram(model, truncate_mode='level', p=3)
+plt.xlabel("Number of points in node (or index of point if no parenthesis).")
+plt.savefig('Figures/Cluster_analysis/Dendrogram_hierarch_clustering.png')
